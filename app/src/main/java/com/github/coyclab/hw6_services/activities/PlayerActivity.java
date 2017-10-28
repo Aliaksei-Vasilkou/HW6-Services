1d628a8d3c0fa09be5c612561dbdf79b9a37950e
Vasilkov Alexey <coyc@inbox.ru>
2017-10-28 23:59:09 +0300

    Tried send data from other activity using method onActivityResult

--- app/src/main/java/com/github/coyclab/hw6_services/activities/PlayerActivity.java
+++ app/src/main/java/com/github/coyclab/hw6_services/activities/PlayerActivity.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.github.coyclab.hw6_services.R;
 import com.github.coyclab.hw6_services.fragments.ControlPanelFragment;
@@ -16,6 +20,8 @@ public class PlayerActivity extends Activity {
     private Button mBtnShowControlPanel;
     private FragmentManager mFragmentManager;
     private final String CONTROL_PANEL_TAG = "ControlPanel";
+    private ImageView mImageView;
+    Button takePicture;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -23,6 +29,15 @@ public class PlayerActivity extends Activity {
         setContentView(R.layout.activity_player);
 
         mFragmentManager = getFragmentManager();
+        mImageView = findViewById(R.id.action_image);
+        takePicture = findViewById(R.id.take_picture);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View pView) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         mBtnShowControlPanel = findViewById(R.id.btn_show_control_panel);
         mBtnShowControlPanel.setOnClickListener(new View.OnClickListener() {
@@ -55,5 +70,25 @@ public class PlayerActivity extends Activity {
         transaction.remove(fragment).commit();
 
         mBtnShowControlPanel.setText(R.string.btn_show_control_panel_name);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+
+
     }
 }
